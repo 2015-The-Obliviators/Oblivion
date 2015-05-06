4e9d5394f89b5cac0b01737923804b5212b9bdf0
Kevin Lawrence <kevinlawrence@outlook.com>
2015-05-06 15:43:23 -0700

    Clean out olde code

---
+++ src/oblivion/BlockLetterI.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oblivion;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author kevin.lawrence
+ */
+public final class BlockLetterI extends BlockLetter {
+
+//<editor-fold defaultstate="collapsed" desc="Constructors">
+    public BlockLetterI(int x, int y, int width, int height, boolean stationary,
+            AccelerationProviderIntf accelerationProvider) {
+        super(x, y, width, height, stationary, accelerationProvider);
+        
+        topBar = new BlockLetterPart(x, y, width, height / 5);
+        stem = new BlockLetterPart(x, y, width / 3, height * 3 / 5);
+        bottomBar = new BlockLetterPart(x, y, width, height / 5);
+        
+        topBar.setFillColor(Color.YELLOW);
+        stem.setFillColor(Color.BLUE);
+        bottomBar.setFillColor(Color.CYAN);
+        
+        getParts().add(topBar);
+        getParts().add(stem);
+        getParts().add(bottomBar);
+        
+        stem.setConnectionUpdateHandler(new ConnectionUpdateHandlerIntf() {
+            @Override
+            public void onUpdate(Rectangle connector, Rectangle connected) {
+                connected.setLocation(connector.x + (connector.width / 2) - (connected.width / 2), connector.y + connector.height);
+            }
+        });
+        
+        bottomBar.setConnectionUpdateHandler(new ConnectionUpdateHandlerIntf() {
+            @Override
+            public void onUpdate(Rectangle connector, Rectangle connected) {
+                connected.setLocation(connector.x + (connector.width / 2) - (connected.width / 2), connector.y + connector.height);
+            }
+        });
+        
+        topBar.registerConnectionListeners(stem);
+        stem.registerConnectionListeners(bottomBar);
+        
+        setPosition(x, y);
+    }
+//</editor-fold>
+
+//<editor-fold defaultstate="collapsed" desc="Movement Methods">
+    @Override
+    public void grow(Direction direction) {
+        stem.height += 1;
+        
+        if (direction == Direction.UP) {
+            setPosition(getPosition().x, getPosition().y - 1);
+        } else {
+            setPosition(getPosition().x, getPosition().y);
+        }
+    }
+    
+    @Override
+    public void shrink(Direction direction) {
+        stem.height = Math.max(stem.height -1, 0);
+        
+        if (direction == Direction.UP) {
+            setPosition(getPosition().x, getPosition().y + 1);
+        } else {
+            setPosition(getPosition().x, getPosition().y);
+        }
+    }
+//</editor-fold>
+       
+//<editor-fold defaultstate="collapsed" desc="Properties">
+    private BlockLetterPart topBar, stem, bottomBar;
+    
+    
+    /**
+     * @param position the position to set
+     */
+    @Override
+    public void setPosition(Point position) {
+        super.setPosition(position);
+        topBar.setLocation(position.x, position.y);
+    }
+
+//</editor-fold>
+    
+}
